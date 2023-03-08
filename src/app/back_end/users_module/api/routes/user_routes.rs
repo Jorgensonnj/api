36663e9077a3d8cf8fa00ffcdf8567cd1c1c2a6e
Nicholas Jorgenson <jorgensonnj@gmail.com>
2023-03-08 04:58:57 -0500

    flatten user routes methods so they do not use so many nested matches

--- src/app/back_end/users_module/api/routes/user_routes.rs
+++ src/app/back_end/users_module/api/routes/user_routes.rs
@@ -13,23 +13,14 @@ pub async fn create_user(
     // unwrap
     let result_pool = data_pool.get_ref();
 
-    // is there a db connection
-    match result_pool {
-        Ok(pool) => {
+    // is there a db connection?
+    let pool = _get_pool(result_pool).await.unwrap();
 
-            // create user
-            let create_result = actions::create(pool, &payload).await;
+    let create_result = actions::create(pool, &payload).await;
 
-            match create_result {
-                Ok(user) => HttpResponse::Ok().json(user),
-                Err(_) => HttpResponse::InternalServerError().finish()
-            }
-        },
-        Err(error) => { 
-            // no db was found
-            tracing::error!("no database found: {:?}", error);
-            HttpResponse::ServiceUnavailable().finish()
-        }
+    match create_result {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(_) => HttpResponse::InternalServerError().finish()
     }
 }
 
@@ -42,23 +33,15 @@ pub async fn read_users(
     // unwrap
     let result_pool = data_pool.get_ref();
 
-    // is there a db connection
-    match result_pool {
-        Ok(pool) => {
+    // is there a db connection?
+    let pool = _get_pool(result_pool).await.unwrap();
 
-            // get all users
-            let read_result = actions::read_all(pool).await;
+    // get all users
+    let read_result = actions::read_all(pool).await;
 
-            match read_result {
-                Ok(users) => HttpResponse::Ok().json(users),
-                Err(_) => HttpResponse::InternalServerError().finish()
-            }
-        },
-        Err(error) => {
-            // no db was found
-            tracing::error!("no database found: {:?}", error);
-            HttpResponse::ServiceUnavailable().finish()
-        }
+    match read_result {
+        Ok(users) => HttpResponse::Ok().json(users),
+        Err(_) => HttpResponse::InternalServerError().finish()
     }
 }
 
@@ -73,23 +56,15 @@ pub async fn read_user(
     let result_pool = data_pool.get_ref();
     let user_id     = user_id.into_inner();
 
-    // is there a db connection
-    match result_pool {
-        Ok(pool) => {
+    // is there a db connection?
+    let pool = _get_pool(result_pool).await.unwrap();
 
-            // get user
-            let read_result = actions::read_one(pool, user_id).await;
+    // get user
+    let read_result = actions::read_one(pool, user_id).await;
 
-            match read_result {
-                Ok(user) => HttpResponse::Ok().json(user),
-                Err(_) => HttpResponse::InternalServerError().finish()
-            }
-       },
-        Err(error) => { 
-            // no db was found
-            tracing::error!("no database found: {:?}", error);
-            HttpResponse::ServiceUnavailable().finish()
-        }
+    match read_result {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(_) => HttpResponse::InternalServerError().finish()
     }
 }
 
@@ -105,47 +80,34 @@ pub async fn update_user(
     let result_pool = data_pool.get_ref();
     let user_id     = user_id.into_inner();
 
-    // is there a db connection
-    match result_pool {
-        Ok(pool) => {
+    // is there a db connection?
+    let pool = _get_pool(result_pool).await.unwrap();
 
-            // get one user
-            let option_user = actions::read_one(pool, user_id).await;
+    // get one user
+    let option_user = actions::read_one(pool, user_id).await;
 
-            match option_user {
-                Ok(user) => {
-
-                let mut json_user = payload.into_inner();
-
-                json_user.username  =
-                    if let Some(u) = json_user.username  { Some(u) } else { Some(user.username)  };
-                json_user.password  =
-                    if let Some(p) = json_user.password  { Some(p) } else { Some(user.password)  };
-                json_user.email     =
-                    if let Some(e) = json_user.email     { Some(e) } else { Some(user.email)     };
-                json_user.discarded =
-                    if let Some(d) = json_user.discarded { Some(d) } else { Some(user.discarded) };
-
-                // update user
-                let update_result = actions::update(pool, user_id, json_user).await;
-
-                    match update_result {
-                        Ok(user) => HttpResponse::Ok().json(user),
-                        Err(_) => HttpResponse::InternalServerError().finish()
-                    }
-                },
-                Err(_) => {
-                    // no user was found
-                    tracing::error!("user_id: {} not found", &user_id);
-                    HttpResponse::NotFound().finish()
-                }
-            }
-        },
-        Err(error) => {
-            // no db was found
-            tracing::error!("no database found: {:?}", error);
-            HttpResponse::ServiceUnavailable().finish()
+    let user = match option_user {
+        Ok(user) => user,
+        Err(_) => {
+            // no user was found
+            tracing::error!("user_id: {} not found", &user_id);
+            return HttpResponse::NotFound().finish();
         }
+    };
+
+    let mut json_user = payload.into_inner();
+
+    json_user.username  = if let Some(u) = json_user.username  { Some(u) } else { Some(user.username)  };
+    json_user.password  = if let Some(p) = json_user.password  { Some(p) } else { Some(user.password)  };
+    json_user.email     = if let Some(e) = json_user.email     { Some(e) } else { Some(user.email)     };
+    json_user.discarded = if let Some(d) = json_user.discarded { Some(d) } else { Some(user.discarded) };
+
+    // update user
+    let update_result = actions::update(pool, user_id, json_user).await;
+
+    match update_result {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(_) => HttpResponse::InternalServerError().finish()
     }
 }
 
@@ -160,22 +122,28 @@ pub async fn delete_user(
     let result_pool = data_pool.get_ref();
     let user_id     = user_id.into_inner();
 
-    // is there a db connection
+    // is there a db connection?
+    let pool = _get_pool(result_pool).await.unwrap();
+
+    // delete user
+    let delete_result = actions::delete(pool, user_id).await;
+
+    match delete_result {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish()
+    }
+}
+
+// helper function
+async fn _get_pool(
+    result_pool: &Result<Pool<Postgres>, Error>
+) -> Result<&Pool<Postgres>, HttpResponse> {
     match result_pool {
-        Ok(pool) => {
-
-            // delete user
-            let delete_result = actions::delete(pool, user_id).await;
-
-            match delete_result {
-                Ok(_) => HttpResponse::Ok().finish(),
-                Err(_) => HttpResponse::InternalServerError().finish()
-            }
-        },
-        Err(error) => {
-            // no db was found
+        // no db was found
+        Ok(pool) => Ok(pool),
+        Err(error) => { 
             tracing::error!("no database found: {:?}", error);
-            HttpResponse::ServiceUnavailable().finish()
-        }
+            Err(HttpResponse::ServiceUnavailable().finish())
+        },
     }
 }
